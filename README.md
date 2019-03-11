16bb61d275b528ec30b725548f0b19ad06a8e851
kairusds <harveyhansjavier@gmail.com>
2019-03-11 10:55:04 +0800

    spectral ruination

---
+++ README.md
@@ -0,0 +1,22 @@
+# eidolonrp
+A rich presence selfbot.
+
+<table>
+<tr><th>ENV</th><th>ENV Description</th></tr>
+<tr><td>token*</th><td>user account token</td></tr>
+<tr><td>appid*</td><td>the application's id to use with its rich presence assets</td></tr>
+<tr><td>name*</td><td>header of the rich presence</td></tr>
+<tr><td>url</td><td>twitch stream url (only used when type=1)</td></tr>
+<tr><td>type*</td><td>(int) type of the activity. 0: playing, 1: streaming, 2: listening, 3: watching</td></tr>
+<tr><td>state*</td><td>second line of the rich presence</td></tr>
+<tr><td>details*</td><td>third line of the rich presence</td></tr>
+<tr><td>partysize*</td><td>(int) party members count</td></tr>
+<tr><td>partymax*</td><td>(int) max party members size</td></tr>
+<tr><td>largeimage*</td><td>rich presence asset large image id</td></tr>
+<tr><td>largetext*</td><td>text tip for the large image</td></tr>
+<tr><td>smallimage*</td><td>rich presence asset small image id</td></tr>
+<tr><td>smalltext*</td><td>text tip for the small image</td></tr>
+</table>
+
+## Preview
+![pv](pv.png)
---
+++ index.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+from os import getenv
+import discord
+import time
+import uuid
+
+print("start")
+bot = commands.Bot(command_prefix="|", self_bot=True)
+
+@bot.event
+async def on_ready():
+	print("Connected!")
+	print(f"Logged in with account {bot.user}.")
+	print("Github: https://github.com/kairusds/eidolonrp")
+
+	ms = time.time() * 1000
+	timestamp = dict(start=ms)
+	assets = dict(
+		large_image=getenv("largeimage"),
+		large_text=getenv("largetext"),
+		small_image=getenv("smallimage"),
+		small_text=getenv("smalltext")
+	)
+	partysize = int(getenv("partysize"))
+	partymax = int(getenv("partymax"))
+	party = dict(id=str(uuid.uuid4()), size=[partysize, partymax])
+	activity = discord.Activity(
+		application_id=getenv("appid"),
+		name=getenv("name"),
+		state=getenv("state"),
+		details=getenv("details"),
+		assets=assets,
+		party=party,
+		timestamps=timestamp,
+		type=int(getenv("type")),
+		url=getenv("url")
+	)
+	await bot.change_presence(activity=activity, status=discord.Status.online)
+	print("Changed rich presence.")
+
+bot.run(getenv("token"), bot=False)
