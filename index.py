5b625d1196fd56bb04209dc637533bf39566d6de
harveyhans <harveyhansjavier@gmail.com>
2021-06-15 09:22:34 +0800

    attempt to make this work

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-from discord.ext import commands
 import discord
 import time
 import uuid
@@ -7,45 +6,44 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-print("boot")
+print("booting...")
 
-bot = commands.Bot(command_prefix="℅", self_bot=True)
+class Eidolon(discord.Client):
+	async def on_ready(self):
+		print("Connected!")
+		print(f"Logged in with account {self.user}.")
+		print("Github: https://github.com/kairusds/eidolonrp")
+		
+		ms = time.time() * 1000
+		timestamp = dict(start=ms)
+		assets = dict(
+			large_image=config["assets"]["largeimage"],
+			large_text=config["assets"]["largetext"],
+			small_image=config["assets"]["smallimage"],
+			small_text=config["assets"]["smalltext"]
+		)
+		
+		activity_args = dict(
+			application_id=config.getint("activity", "appid"),
+			name=config["activity"]["name"],
+			state=config["activity"]["state"],
+			details=config["activity"]["details"],
+			assets=assets,
+			timestamps=timestamp,
+			type=config.getint("activity", "type")
+		)
+		
+		if config.getint("activity", "type") == 1:
+			activity_args["url"] = config["activity"]["url"]
+		
+		if config.getboolean("party", "enabled"):
+			partysize = config.getint("party", "size")
+			partymax = config.getint("party", "maxsize")
+			activity_args["party"] = dict(id=str(uuid.uuid4()), size=[partysize, partymax])
+		
+		activity = discord.Activity(**activity_args)
+		await self.change_presence(activity=activity, status=config["activity"]["status"])
+		print("Changed rich presence.")
 
-@bot.event
-async def on_ready():
-	print("Connected!")
-	print(f"Logged in with account {bot.user}.")
-	print("Github: https://github.com/kairusds/eidolonrp")
-	
-	ms = time.time() * 1000
-	timestamp = dict(start=ms)
-	assets = dict(
-		large_image=config["assets"]["largeimage"],
-		large_text=config["assets"]["largetext"],
-		small_image=config["assets"]["smallimage"],
-		small_text=config["assets"]["smalltext"]
-	)
-	
-	activity_args = dict(
-		application_id=config.getint("activity", "appid"),
-		name=config["activity"]["name"],
-		state=config["activity"]["state"],
-		details=config["activity"]["details"],
-		assets=assets,
-		timestamps=timestamp,
-		type=config.getint("activity", "type")
-	)
-	
-	if config.getint("activity", "type") == 1:
-		activity_args["url"] = config["activity"]["url"]
-	
-	if config.getboolean("party", "enabled"):
-		partysize = config.getint("party", "size")
-		partymax = config.getint("party", "maxsize")
-		activity_args["party"] = dict(id=str(uuid.uuid4()), size=[partysize, partymax])
-	
-	activity = discord.Activity(**activity_args)
-	await bot.change_presence(activity=activity, status=config["activity"]["status"])
-	print("Changed rich presence.")
-
-bot.run(config["user"]["token"], bot=False)
+client = Eidolon()
+client.run(config["user"]["token"])
